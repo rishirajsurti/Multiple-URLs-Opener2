a2a9fd11f8c78d9d3caeea211444d5daf4081e91
Rishiraj Surti <rishirajsurti.iitm@gmail.com>
2016-12-16 13:19:22 +0530

    added files

---
+++ Other/index.js
@@ -0,0 +1,85 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+	//load the sites
+
+	load();
+});*/
+
+/*$(document).ready(function(){
+ 	load();
+ });*/
+
+chrome.browserAction.onClicked.addListener(function(tab) { 
+	//alert("load");
+	load();
+});
+
+//enter your sites here
+var setOne = [
+"https://goo.gl/1tYowK",
+"https://goo.gl/daCnF7",
+"https://goo.gl/dwivmm"
+]
+
+
+//format for new set
+/*
+var newSetName = [
+"",
+"",
+""
+]
+*/
+//now add a snippet in the load() function
+
+function load() {
+	var newWindow;
+
+	var uri = new URI("");
+	var currentUrl = '';
+	
+	//Set One
+	//for first link of first set;
+	uri = URI(setOne[0]);
+	uri.normalize();
+	if(uri.scheme()==''){currentUrl = 'http://'+uri.href();}
+	else{currentUrl=uri.href();}
+
+	//for rest of the links of set
+	chrome.windows.create({url : currentUrl},function(win){	
+								newWindow = win;
+								loadSet(newWindow, setOne);
+							} );
+
+
+	
+	//Format for new set
+
+/*	//for first link of next set;	
+	uri = URI(newSetName[0]); //NOTICE CHANGES HERE
+	uri.normalize();
+	if(uri.scheme()==''){currentUrl = 'http://'+uri.href();}
+	else{currentUrl=uri.href();}
+
+	//for rest of the links of set
+	chrome.windows.create({url : currentUrl},function(win){	
+								newWindow = win;				
+								loadSet(newWindow, newSetName);
+							} );*/ //NOTICE CHANGES HERE
+}
+
+
+function loadSet(currentWindow, urlSet) {
+
+	var uri = new URI("");
+	var currentUrl = '';
+	
+	for(var i = 1; i < urlSet.length; i++){
+		
+		uri = URI(urlSet[i]);
+		uri.normalize();
+		if(uri.scheme()==''){currentUrl = 'http://'+uri.href();}
+		else{currentUrl=uri.href();}
+			
+		chrome.tabs.create({url: currentUrl, selected: false, "windowId":currentWindow.id}); 		
+	}	
+}
---
+++ StackExchange/README.md
@@ -0,0 +1,7 @@
+# Mulitple-URL-opener
+Open multiple urls in dedicated windows at a click.
+
+To install it, choose 'Load Unpacked Extenstion' on Extension Tab in Google Chrome and point it to the source folder.  
+
+To edit the URLs you want to open, go to index.js file.  
+A format has been provided to add new URLs.  
